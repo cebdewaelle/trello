a9b92579a13026f6e72ee5de48485bdc3e039aeb
Sebastien Dewaelle <sdewaelle@gmail.com>
2025-04-30 11:46:08 +0200

    Ajout des fonctions de colonnes

---
+++ src/colonne.py
@@ -0,0 +1,17 @@
+import board
+
+
+def create_colonne(board: dict, nom_colonne: str) -> dict:
+    board[nom_colonne] = []
+    return board
+
+
+def update_colonne(board: dict, old_nom_colonne: str, new_nom_colonne: str) -> dict:
+    board[new_nom_colonne] = board.pop(old_nom_colonne)
+    return board
+
+
+def delete_colonne(board: dict, nom_colonne: str) -> dict:
+    del board[nom_colonne]
+    return board
+
